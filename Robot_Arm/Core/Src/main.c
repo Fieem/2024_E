84389b5f3b94b5c17f7517782d1d1b6e8e03b68a
Qinhuan <wenhaozhang1939@outlook.com>
2026-07-14 11:23:14 +0800

    修复成功

--- Robot_Arm/Core/Src/main.c
+++ Robot_Arm/Core/Src/main.c
@@ -20,7 +20,6 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "can.h"
-#include "dma.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -93,7 +92,6 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
   MX_CAN1_Init();
   MX_TIM2_Init();
   MX_USART1_UART_Init();
